838c07341f04ad4bed89f34b78bad54bb4102ade
sadaf953 <sadafmahvishedu@gmail.com>
2026-08-23 23:21:16 +0530

    Updates

--- assets/chevron-left-BfGWdIIl.js
+++
@@ -1,1 +0,0 @@
-import{gt as e}from"./index-Dk0tnwen.js";var t=e(`ChevronLeft`,[[`path`,{d:`m15 18-6-6 6-6`,key:`1wnfg3`}]]);export{t};
--- assets/chevron-right-BwvtBZUZ.js
+++
@@ -1,1 +0,0 @@
-import{gt as e}from"./index-Dk0tnwen.js";var t=e(`ChevronRight`,[[`path`,{d:`m9 18 6-6-6-6`,key:`mthhwq`}]]);export{t};
