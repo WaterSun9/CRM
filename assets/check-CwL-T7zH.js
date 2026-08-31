f1776fd4b05ed605e10a188963aea1c32a54d297
sadaf953 <sadafmahvishedu@gmail.com>
2026-08-31 19:14:14 +0530

    Updates

--- assets/check-CwL-T7zH.js
+++
@@ -1,1 +0,0 @@
-import{Ot as e}from"./index-Dbo8_h3w.js";var t=e(`Check`,[[`path`,{d:`M20 6 9 17l-5-5`,key:`1gmf2c`}]]);export{t};
--- assets/chevron-up-0UfPR3-b.js
+++
@@ -1,1 +0,0 @@
-import{Ot as e}from"./index-Dbo8_h3w.js";var t=e(`ChevronUp`,[[`path`,{d:`m18 15-6-6-6 6`,key:`153udz`}]]);export{t};
