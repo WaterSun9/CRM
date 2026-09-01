6c5ecdfbd66b6014cacc959014eaf6264bb73861
sadaf953 <sadafmahvishedu@gmail.com>
2026-09-01 20:34:21 +0530

    Updates

--- assets/chevron-right-BTKcQrGD.js
+++
@@ -1,1 +0,0 @@
-import{At as e}from"./index-Xu566HvZ.js";var t=e(`ChevronRight`,[[`path`,{d:`m9 18 6-6-6-6`,key:`mthhwq`}]]);export{t};
--- assets/loader-circle-DMM-zSgH.js
+++
@@ -1,1 +0,0 @@
-import{At as e}from"./index-Xu566HvZ.js";var t=e(`LoaderCircle`,[[`path`,{d:`M21 12a9 9 0 1 1-6.219-8.56`,key:`13zald`}]]);export{t};
